51fc6e0f903dc78df50f4cc2de2faa8088eaf79f
Warren Denzel F. Cheng <warrendenzel.cheng@gcash.com>
2025-07-16 14:09:51 +0800

    Added challenge lab 1 project files

---
+++ session1/challenge-lab1/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.demo.myfirstapi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+        @Value("${app.greeting.message}") // Inject value from application.properties
+        private String customGreeting;
+
+        @GetMapping("/hello")
+        public String sayHello() {
+            return customGreeting; // Now returns the custom message
+        }
+
+    @GetMapping("/greet/{name}")
+    public String greetUser(@PathVariable String name) {
+        return "Greetings, " + name + "!";
+    }
+}
